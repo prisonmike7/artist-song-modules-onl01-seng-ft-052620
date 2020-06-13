126915d0a7430a50afae207b430967ff79c79b09
Michael Harris <mwharris7@gmail.com>
2020-06-13 14:27:27 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,19 @@
 module Memorable
 
-  def reset_all
-    all.clear
-  end
+  module ClassMethods
+      def reset_all
+        self.all.clear
+      end
 
-  def count
-    all.count
-  end
+      def count
+        self.all.count
+      end
+    end
+
+    module InstanceMethods
+      def initialize
+        # some more code coming soon!
+      end
+    end
 
 end
